688d54c8db341856dd338b4150b3abacb40d505c
enzohideo <enzohideo@users.noreply.github.com>
2024-08-24 00:31:14 -0300

    refactor(flake): Override packages in the let expression

--- flake.nix
+++ flake.nix
@@ -13,36 +13,33 @@
     inherit (nixpkgs) lib;
     eachSystem = lib.genAttrs (import systems);
     pkgsFor = eachSystem (system: nixpkgs.legacyPackages.${system});
-    jdkFor = eachSystem (system: (import inputs.nixpkgs-jdk {
-      inherit system;
-    }).jdk7.overrideAttrs (prevAttrs: {
-      DISABLE_HOTSPOT_OS_VERSION_CHECK = "ok";
-      preFixup = prevAttrs.preFixup + ''
-        cat <<EOF > $out/nix-support/setup-hook
-        if [ -z "\''${JAVA_HOME-}" ]; then export JAVA_HOME=$out/lib/openjdk; fi
-        EOF
-      '';
-    }));
+    jdkFor = eachSystem (system: (import inputs.nixpkgs-jdk { inherit system; }));
   in {
     packages = eachSystem (system: let
       pkgs = pkgsFor.${system};
+      jdk = jdkFor.${system}.jdk7.overrideAttrs (prevAttrs: {
+        DISABLE_HOTSPOT_OS_VERSION_CHECK = "ok";
+        preFixup = prevAttrs.preFixup + ''
+          cat <<EOF > $out/nix-support/setup-hook
+          if [ -z "\''${JAVA_HOME-}" ]; then export JAVA_HOME=$out/lib/openjdk; fi
+          EOF
+        '';
+      });
+      ant = pkgs.ant.overrideAttrs (prevAttrs: rec {
+        version = "1.9.16";
+
+        src = pkgs.fetchurl {
+          url = "mirror://apache/ant/binaries/apache-ant-${version}-bin.tar.bz2";
+          sha256 = "0rif9kj6njajy951w3aapk27y1mbaxb08whs126v533h96rb1kjp";
+        };
+
+        contrib = pkgs.fetchurl {
+          url = "mirror://sourceforge/ant-contrib/ant-contrib-1.0b3-bin.tar.bz2";
+          sha256 = "96effcca2581c1ab42a4828c770b48d54852edf9e71cefc9ed2ffd6590571ad1";
+        };
+      });
     in builtins.mapAttrs (name: value: pkgs.callPackage ./packages/${name} value) {
-      lejos-nxj = {
-        jdk = jdkFor.${system};
-        ant = pkgs.ant.overrideAttrs (prevAttrs: rec {
-          version = "1.9.16";
-
-          src = pkgs.fetchurl {
-            url = "mirror://apache/ant/binaries/apache-ant-${version}-bin.tar.bz2";
-            sha256 = "0rif9kj6njajy951w3aapk27y1mbaxb08whs126v533h96rb1kjp";
-          };
-
-          contrib = pkgs.fetchurl {
-            url = "mirror://sourceforge/ant-contrib/ant-contrib-1.0b3-bin.tar.bz2";
-            sha256 = "96effcca2581c1ab42a4828c770b48d54852edf9e71cefc9ed2ffd6590571ad1";
-          };
-        });
-      };
+      lejos-nxj = { inherit jdk; inherit ant; };
     });
 
     devShells = eachSystem (system: let
