f19e83fcdc097cade4cc9bd60ff447af3509e0e0
enzohideo <enzohideo@users.noreply.github.com>
2024-08-23 23:44:38 -0300

    flake: Add jdtls to dev shell

--- flake.nix
+++ flake.nix
@@ -47,14 +47,35 @@
 
     devShells = eachSystem (system: let
       pkgs = pkgsFor.${system};
-    in (lib.attrsets.mergeAttrsList (builtins.map (name: {
+    in (lib.attrsets.mergeAttrsList (builtins.map (name: let
+      lejos = self.packages.${system}.${name};
+    in {
       ${name} = pkgs.mkShell {
         name = "${name}-shell";
-        buildInputs = [
-          pkgs.jre
-          self.packages.${system}.${name}
+        buildInputs = with pkgs; [
+          jre
+          jdt-language-server
+        ] ++ [
+          lejos
         ];
         shellHook = ''
+          cat <<EOF > pom.xml
+          <project>
+            <modelVersion>4.0.0</modelVersion>
+            <groupId>${name}-project</groupId>
+            <artifactId>${name}-project</artifactId>
+            <version>1</version>
+            <dependencies>
+              <dependency>
+               <groupId>${name}</groupId>
+               <artifactId>${name}</artifactId>
+               <scope>system</scope>
+               <version>1</version>
+               <systemPath>${lejos}/lib/nxt/classes.jar</systemPath>
+              </dependency>
+            </dependencies>
+          </project>
+          EOF
           . ./packages/${name}/utils.sh
         '';
       };
