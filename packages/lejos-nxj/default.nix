e0f03654efda5309193653e1f9dc2d1dbc161ce0
enzohideo <enzohideo@users.noreply.github.com>
2024-09-21 12:46:44 -0300

    lejos-nxj: Add udev rule

--- packages/lejos-nxj/default.nix
+++ packages/lejos-nxj/default.nix
@@ -1,11 +1,17 @@
 {
   stdenv,
   fetchurl,
+  writeText,
   ant,
   jdk,
   libusb-compat-0_1,
 }:
-
+let
+  udev-rule = writeText "lejos-nxj-udev-rule" ''
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="0694", ATTRS{idProduct}=="0002", GROUP="lego", MODE="0664"
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="6124", GROUP="lego", MODE="0664"
+  '';
+in
 stdenv.mkDerivation rec {
   pname = "lejos-nxj";
   version = "0.9.1beta-3";
@@ -36,6 +42,9 @@ stdenv.mkDerivation rec {
 
     mkdir -p $out
     cp -pr --reflink=auto ./{bin,lib} $out
+
+    mkdir -p $out/lib/udev/rules.d
+    cp -p --reflink=auto ${udev-rule} $out/lib/udev/rules.d/70-lego-nxj.rules
 
     runHook postInstall
   '';
