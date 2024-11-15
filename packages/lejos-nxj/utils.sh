33dea64d057e326c916072f613d57c3631760238
enzohideo <enzohideo@users.noreply.github.com>
2024-11-14 22:22:43 -0300

    devshell: Add error/success messages to utils commands

--- packages/lejos-nxj/utils.sh
+++ packages/lejos-nxj/utils.sh
@@ -4,16 +4,20 @@
 up() {
   filename="$1"
   basename="${filename%.*}"
-  nxjc "$filename"
-  nxj -r "$basename"
+  nxjc "$filename" \
+    && nxj "$basename" "$@" \
+    && echo -e '\e[32mSuccess' \
+    || echo -e '\e[31mFailed'
 }
 
 # Compile and run a PC API program
 pc() {
   filename="$1"
   basename="${filename%.*}"
-  nxjpcc "$filename"
-  nxjpc "$basename"
+  nxjpcc "$filename" \
+    && nxjpc "$basename" "$@" \
+    && echo -e '\e[32mSuccess' \
+    || echo -e '\e[31mFailed'
 }
 
 # Removes .class files generated during compilation
