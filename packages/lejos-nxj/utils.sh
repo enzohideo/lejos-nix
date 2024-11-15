ef72869f3bc3bcb5602a105b688717c0f6d480b3
enzohideo <enzohideo@users.noreply.github.com>
2024-11-14 22:45:06 -0300

    fix(package/lejos-nxj): Fix utils' compilation commands

--- packages/lejos-nxj/utils.sh
+++ packages/lejos-nxj/utils.sh
@@ -5,7 +5,7 @@ up() {
   filename="$1"
   basename="${filename%.*}"
   nxjc "$filename" \
-    && nxj "$basename" "$@" \
+    && nxj "$basename" ${@:2} \
     && echo -e '\e[32mSuccess' \
     || echo -e '\e[31mFailed'
 }
@@ -15,7 +15,7 @@ pc() {
   filename="$1"
   basename="${filename%.*}"
   nxjpcc "$filename" \
-    && nxjpc "$basename" "$@" \
+    && nxj -o "${basename}.nxj" "${basename}" ${@:2} \
     && echo -e '\e[32mSuccess' \
     || echo -e '\e[31mFailed'
 }
